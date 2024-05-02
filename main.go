154a993e24475fc967acb3c802cc44d0bd73bb50
Daniel Steman <daniel-steman@live.nl>
2024-05-02 22:06:52 +0200

    cobra init

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-  "github.com/danielsteman/golab/cmd"
-)
+import "github.com/danielsteman/golab/cmd"
 
 func main() {
-  cmd.Execute()
+	cmd.Execute()
 }
